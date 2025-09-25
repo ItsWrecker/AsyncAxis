69e6dd9e7daca747d54a6acdf4f961b52cf20ab7
wrecker <santosh.mehta@live.com>
2025-09-25 10:44:38 +0530

    - README.md added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,10 @@ android {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
+        debug {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
